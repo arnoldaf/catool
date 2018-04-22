7d7e8a199a62425ffe8c82b8a50d5b42dbc66e3d
arnoldaf <arnoldaf88@gmail.com>
2018-04-22 15:38:18 +0530

    Verify domain added

--- app/Http/Middleware/VerifyClientsDomain.php
+++ app/Http/Middleware/VerifyClientsDomain.php
@@ -14,20 +14,21 @@ class VerifyClientsDomain {
      * @var array
      */
     public function handle($request, Closure $next) {
-        //to get client from domain
-        $token = trim(str_replace('Bearer', '', $request->header('Authorization')));
-        $loggedIn = JWTAuth::authenticate($token);
-        if ($loggedIn && $loggedIn->role_id > 0) {
-            $pId = $loggedIn->p_id;
-            $clientDomain = (!$request->header('domain')) ? env(DEFAULT_DOMAIN, 'localhost:4200') : str_replace(['http://', 'https://'], '', $request->header('domain'));
-            $clientInfo = UserDomainConfig::whereIn('user_id', [$loggedIn->id, $pId])
-                    ->where('domain_name', $clientDomain)
-                    ->first();
-            if (!$clientInfo) {
-                return response()->json(['error' => ['user' => trans('User not found')]], 404);
+        if ($request->header('authorization') != null) {
+            //to get client from domain
+            $token = trim(str_replace('Bearer', '', $request->header('authorization')));
+            $loggedIn = JWTAuth::authenticate($token);
+            if ($loggedIn && $loggedIn->role_id > 0) {
+                $pId = $loggedIn->p_id;
+                $clientDomain = (!$request->header('domain')) ? env(DEFAULT_DOMAIN, 'localhost:4200') : str_replace(['http://', 'https://'], '', $request->header('domain'));
+                $clientInfo = UserDomainConfig::whereIn('user_id', [$loggedIn->id, $pId])
+                        ->where('domain_name', $clientDomain)
+                        ->first();
+                if (!$clientInfo) {
+                    return response()->json(['error' => ['user' => trans('User not found')]], 404);
+                }
             }
         }
-        
         return $next($request);
     }
 
