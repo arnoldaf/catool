7415dfccd0921910cfe2df0169f73854756007a6
Paul Diaz <pramod.swe97@gmail.com>
2018-05-01 08:11:25 +0530

    CA user crud work updated

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -66,35 +66,48 @@ class UserService {
      * @param  int  $id
      * @return Response
      */
-     public function getUsers($id) {
-       if ($id != null) {
-           $users = Users::find($id);
-       } else {
-           // $users = Users::all();
-           $users = Users:: where('p_id','2')->orderBy('id','desc')->get();
-       }
-     
-       return $users->toArray();
+    public function getUsers($id) {
+        if ($id != null) {
+            $users = Users::find($id);
+        } else {
+            // $users = Users::all();
+            $users = Users:: where('p_id', '2')->orderBy('id', 'desc')->get();
+        }
+
+        return $users->toArray();
     }
 
-
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function getCaUsers($id) {
+        if ($id != null) {
+            $users = Users::find($id);
+        } else {
+            // $users = Users::all();
+            $users = Users:: where('p_id', '0')->orderBy('id', 'desc')->get();
+        }
+        return $users->toArray();
+    }
 
     /**
-    * Remove the specified resource from storage.
-    *
-    * @param  int  $id
-    * @return Response
-    */
-   public function deleteUsers($request) {
-       $id = $request->input('id');
-       // delete
-       $users = Users::find($id);
-       $users->delete();
-       return $users->toArray();
-   }
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function deleteUsers($request) {
+        $id = $request->input('id');
+        // delete
+        $users = Users::find($id);
+        $users->delete();
+        return $users->toArray();
+    }
 
-	
-	public function forgotPassword($request) {
+    public function forgotPassword($request) {
         $messges = [];
         $validator = Validator::make($request->all(), [
                     'email' => 'required|email|max:255,email'
@@ -128,8 +141,8 @@ class UserService {
             return ['result' => true, 'message' => 'Email Not Available'];
         }
     }
-	
-	 public function updatePassword($request) {
+
+    public function updatePassword($request) {
         $messges = [];
         $validator = Validator::make($request->all(), [
                     'password' => 'required|min:6',
@@ -148,77 +161,73 @@ class UserService {
         }
         $token = $request->input('forgot_token');
         $count = Users::where('forgot_token', '=', $token)->count();
-       
+
         if ($count > 0) {
-            DB::table('users')->where('forgot_token', $token)->update(['password' => 'ghh'] );
-            DB::table('users')->where('forgot_token', $token)->update(['forgot_token' => ''] );
+            DB::table('users')->where('forgot_token', $token)->update(['password' => 'ghh']);
+            DB::table('users')->where('forgot_token', $token)->update(['forgot_token' => '']);
             return ['result' => true, 'message' => 'Password updated successfully'];
         }
     }
-    
-    
-    
-     /**
-    * Show the form for creating a new resource.
-    *
-    * @return Response
-    */
-   public function addUser($request) {
-       
-               $messges = [];
-               $validator = Validator::make($request->all(), [
-                           'firstname' => 'required|max:255',
-                           'lastname' => 'required|max:255',
-                           'email' => 'required|email|max:255|unique:users,email,' . $request->input('id'),
-                           'phonenumber' => 'required|max:10|unique:users,mobile,' . $request->input('id'),
-                           //'password' => 'min:6',
-                           //'password_confirmation' => 'required_with:password|same:password|min:6'
-                               ], [
-                           'firstname.required' => 'First name is required.',
-                           'lastname.required' => 'Last name is required.',
-                           'email.required' => 'We need to know your e-mail address!',
-                           'phonenumber.required' => 'We need to know your mobile!',
-                           'password.required' => 'Password is required!',
-                           'password_confirmation.required' => 'Confirm Password must be same as password!',
-                       ]
-               );
-               
-               if ($validator->fails()) {
-                   foreach ($validator->errors()->getMessages() as $key => $value) {
-                        $messges[$key] = $value[0];
-                   }
-                   return ['errors' => $messges];
-               }
-              // print_r( $request->all());
-               //DB::enableQueryLog();  
-               $Users = ($request->input('id') > 0 ) ? Users::find($request->input('id')) : new Users;
-               $Users->first_name = $request->input('firstname');
-               $Users->last_name = $request->input('lastname');
-               $Users->email = $request->input('email');
-               $Users->mobile = $request->input('phonenumber');
-               $Users->user_type_id = $request->input('client_type');
-               $Users->ip_address = $request->ip();
-               $Users->p_id = $this->getLoginId($request);
-               $Users->status = 1;
-               if($request->input('password') != '') {
-                $Users->password = bcrypt($request->input('password'));
-               }
-               //->toSql()
-                       
-               $Users->save();
-               
-              //dd(DB::getQueryLog());  
-               return ['data'>$Users->toArray(), 'errors' => ''];
-               //return $Users->toArray();
-       }
-       
-       
+
     /**
-    * parse Token
-    * @return type array
-    */
-    public function parseToken()
-    {
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function addUser($request) {
+
+        $messges = [];
+        $validator = Validator::make($request->all(), [
+                    'firstname' => 'required|max:255',
+                    'lastname' => 'required|max:255',
+                    'email' => 'required|email|max:255|unique:users,email,' . $request->input('id'),
+                    'phonenumber' => 'required|max:10|unique:users,mobile,' . $request->input('id'),
+                        //'password' => 'min:6',
+                        //'password_confirmation' => 'required_with:password|same:password|min:6'
+                        ], [
+                    'firstname.required' => 'First name is required.',
+                    'lastname.required' => 'Last name is required.',
+                    'email.required' => 'We need to know your e-mail address!',
+                    'phonenumber.required' => 'We need to know your mobile!',
+                    'password.required' => 'Password is required!',
+                    'password_confirmation.required' => 'Confirm Password must be same as password!',
+                        ]
+        );
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->getMessages() as $key => $value) {
+                $messges[$key] = $value[0];
+            }
+            return ['errors' => $messges];
+        }
+        // print_r( $request->all());
+        //DB::enableQueryLog();  
+        $Users = ($request->input('id') > 0 ) ? Users::find($request->input('id')) : new Users;
+        $Users->first_name = $request->input('firstname');
+        $Users->last_name = $request->input('lastname');
+        $Users->email = $request->input('email');
+        $Users->mobile = $request->input('phonenumber');
+        $Users->user_type_id = $request->input('client_type');
+        $Users->ip_address = $request->ip();
+        $Users->p_id = $this->getLoginId($request);
+        $Users->status = 1;
+        if ($request->input('password') != '') {
+            $Users->password = bcrypt($request->input('password'));
+        }
+        //->toSql()
+
+        $Users->save();
+
+        //dd(DB::getQueryLog());  
+        return ['data' > $Users->toArray(), 'errors' => ''];
+        //return $Users->toArray();
+    }
+
+    /**
+     * parse Token
+     * @return type array
+     */
+    public function parseToken() {
         return $payload = JWTAuth::parseToken()->getPayload();
     }
 
@@ -226,23 +235,92 @@ class UserService {
      * get parent id 
      * @return type int
      */
-    public function getParentId($request)
-    {
-        $token = str_replace('Bearer ',  '', $request->header('Authorization'));
+    public function getParentId($request) {
+        $token = str_replace('Bearer ', '', $request->header('Authorization'));
         $user = JWTAuth::authenticate($token);
         return $user->pid;
     }
+
     /**
      * get whitelabel id 
      * @return type int
      */
-    public function getLoginId($request)
-    {
-        $token = str_replace('Bearer ',  '', $request->header('Authorization'));
+    public function getLoginId($request) {
+        $token = str_replace('Bearer ', '', $request->header('Authorization'));
         $user = JWTAuth::authenticate($token);
         return $user->id;
     }
 
-       
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function addCaUser($request) {
+
+        
+        $messges = [];
+        $validator = Validator::make($request->all(), [
+                    'first_name' => 'required|max:255',
+                    'last_name' => 'required|max:255',
+                    'email' => 'required|email|max:255|unique:users,email,' . $request->input('id'),
+                    'url' => 'required|max:255|unique:users,url,' . $request->input('id'),
+                    'mobile' => 'required|max:10|unique:users,mobile,' . $request->input('id'),
+                    'password' => 'min:6',
+                    'password_confirmation' => 'required_with:password|same:password|min:6'
+                   ], [
+                    'firstname.required' => 'First name is required.',
+                    'lastname.required' => 'Last name is required.',
+                    'email.required' => 'We need to know your e-mail address!',
+                    'phonenumber.required' => 'We need to know your mobile!',
+                    'password.required' => 'Password is required!',
+                    'password_confirmation.required' => 'Confirm Password must be same as password!',
+                   ]
+        );
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->getMessages() as $key => $value) {
+                $messges[$key] = $value[0];
+            }
+            return ['errors' => $messges];
+        }
+        // print_r( $request->all());
+        //DB::enableQueryLog();  
+        $Users = ($request->input('id') > 0 ) ? Users::find($request->input('id')) : new Users;
+        $Users->first_name      = $request->input('first_name');
+        $Users->middle_name     = $request->input('middle_name');
+        $Users->last_name       = $request->input('last_name');
+        $Users->user_type_id    = $request->input('user_type_id');
+        $Users->url             = $request->input('url');
+        $Users->client_code     = $request->input('client_code');
+        $Users->email           = $request->input('email');
+        $Users->mobile          = $request->input('mobile');
+        $Users->phone           = $request->input('phone');
+        $Users->personal_email  = $request->input('personal_email');
+        $Users->address         = $request->input('address');
+        $Users->city_name            = $request->input('city');
+        $Users->state_id        = $request->input('state_id');
+        $Users->county_id       = $request->input('county_id');
+        $Users->zip_code        = $request->input('zip_code');
+        $Users->office_address  = $request->input('office_address');
+        $Users->office_phone    = $request->input('office_phone');
+        $Users->gst_number      = $request->input('gst_number');
+        $Users->pan_number      = $request->input('pan_number');
+        $Users->adhar_number    = $request->input('adhar_number');
+        $Users->brand_name      = $request->input('brand_name');
+        $Users->referral_code   = $request->input('referral_code');
+        $Users->ip_address      = $request->ip();
+        $Users->p_id            = 0;
+        $Users->status          = $request->input('status');
+        if ($request->input('password') != '') {
+            $Users->password = bcrypt($request->input('password'));
+        }
+        //->toSql()
+        
+        $Users->save();
+        //dd(DB::getQueryLog());  
+        return ['data' > $Users->toArray(), 'errors' => ''];
+        //return $Users->toArray();
+    }
 
 }
